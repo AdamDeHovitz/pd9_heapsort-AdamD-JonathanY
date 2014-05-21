07653da8733db261b8150c9ba370b277e0665eef
AdamDeHovitz <adam.dehovitz@gmail.com>
2014-05-20 22:14:22 -0400

    Working on HeapSort
    There’s an issue with my heapify method that defies solution :(

--- AdamD/HeapSort.java
+++ AdamD/HeapSort.java
@@ -1,29 +1,38 @@
 // Adam DeHovitz
 // Period 9
-// HW #41
-// 2014-05-19
-
+// HW #42
+// 2014-05-20
 
 public class HeapSort{
-
-    /*
-In the best case, it will already be in order from min to max, which would decrease the time to input but still lead to nlogn run time due to remove.
-In the worse case it would be reversed order, but stil nlogn runtime.
-
-    */
-
     
     public static Integer[] sort( Integer[] data ){
-	ALHeap heap = new ALHeap();
-	for (Integer x: data){
-	    heap.add(x);
+	heapify(data);
+	//return sort(data);
+	return data;
+    }
+    public static void heapify( Integer[] data){ //This just barely doesn't work :/
+	for (int x = data.length - 1; x >= 0; x--){
+	    int z = x;
+	    while (z > 0){
+		if (data[z] < data[(z-1)/2]){
+		    swap(data,z,(z-1)/2);
+		    z = (z-1)/2;
+		}
+		else{
+		    break;}
+	    }
 	}
-	Integer ret[] = new Integer[data.length];
-	for(int x = 0; x < data.length; x++){
-		ret[x] = heap.removeMin();
-	}
-	return ret;
-    } //O(nlogn)
+	
+	//System.out.println(data);
+}
+    public static void swap(Integer[] data, int ind1, int ind2){
+	int temp = data[ind1];
+	data[ind1] = data[ind2];
+	data[ind2] = temp;
+    }
+
+
+     //O(nlogn)
 
     public static String Print( Integer[] A){ //helper
 	String ans = "";
@@ -32,9 +41,13 @@ In the worse case it would be reversed order, but stil nlogn runtime.
 	return ans;}
 
     public static void main( String[] args){
-	Integer[] data = {2,35,5,3,23,1,3,4,1,24,7,5,84,53,52,3,};
+	Integer[] data = {2,35,5,3,23,4,1,24,7,5,84,53,52};
 	data = sort(data);
 	System.out.println(Print(data));
+	Integer[] other = {89,3,35,2,4,7,1};
+	other = sort(other);
+	System.out.println(Print(other));
+
 
 
     }
